369d12f5a5cabc44254be8188d2394c8d451a1c9
Myron <dubtwee@gmail.com>
2021-01-25 11:51:19 -0600

    initial commit

---
+++ review.js
@@ -0,0 +1,72 @@
+/*
+JARGON
+. - dot
+() - parenthesis, parens
+[] - brackets, square brackets, square bracks, square braces
+{} - curly bracks, braces, curlies, curly brackets
+< - less than, bird,  angle brackets
+> - greater than, gator, angle brackets
+/ - whack, slash, forward slash
+\ - back slash
+! - not, bang
+# - pound, hash, number
+* - splat, star
+|| - pipes, double pipes, or
+$ - bling, dollar sign, cash
+NaN - not a number, bread
+*/
+
+/*
+Data Types
+
+numbers
+0 1 2 3
+
+// native functions
+parseInt()
+parseFloat()
+
+strings
+'120' 'cat' '*'
+
+booleans (flag)
+true
+false
+*/
+
+// VARIABLES
+// const - cannot reset value
+// let - reset value
+// var - DO NOT WANT YOU TO USE THIS
+
+const product = 'orange';
+let firstName = 'Myron';
+firstName = 'Chad';
+
+// let - keyword
+// name - name anything it's kittyKat
+// = - assignment operator
+// 'myron' - value
+
+const foo = 'string';
+const bar = 29;
+
+let myNumber = 3;
+
+myNumber = myNumber + 1; // 4
+
+myNumber += 1; // 5
+myNumber -= 1; // 4
+
+myNumber++; // auto add 1
+myNumber--; // auto minus 1
+
+// + is adding
+// - is subtracting
+// * is multiply
+// / is divide
+
+// Concatenation
+
+const myNewNumber = 1 + 2; //3
+console.log(myNewNumber);
