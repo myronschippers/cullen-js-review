1603e9eb78d9575d93c42511f1fe6602cc3a2908
Myron <dubtwee@gmail.com>
2021-01-25 14:28:09 -0600

    functions and loops

--- review.js
+++ review.js
@@ -76,3 +76,99 @@ const myDivide = '36' / 6;
 console.log(myDivide);
 const myMultiply = 'ten' * 3;
 console.log(myMultiply);
+
+// NaN - not a number
+// null - string false
+let lastName = 'Myron';
+// undefined
+
+// no defined
+
+// FALSY
+// ''
+// 0
+// null
+// undefined
+// NaN
+
+// CONDITIONALS
+if (lastName !== 'Myron') {
+  // executed code
+  console.log('Yippy');
+} else {
+  // if expression is false run code
+  console.log('IT WAS FALSY');
+}
+
+// == - only matches value
+// === - matches value and type
+// != - only not value
+// !== - both not value and type
+
+const numComp1 = '8';
+const numComp2 = '10';
+
+if (numComp1 > numComp2) {
+  console.log('Comp 1 WINS!!!!');
+} else if (numComp1 < numComp2) {
+  console.log('Comp 2 WINS!!!!');
+} else {
+  console.log('Nothing WINS?!?!?!');
+}
+
+// >
+// <
+// >=
+// <=
+
+// LOOPS
+
+let whileValue = 8;
+
+// while
+while (/* expression - evaluates to truthy */ whileValue >= 0) {
+  // code block - runs every iteration
+  console.log('WHILE', whileValue);
+  whileValue--;
+}
+
+console.log('AFTER WHILE:', whileValue);
+
+// let masterNumber = 2;
+let hatList = ['stocking cap', 'baseball cap', 'fedora', 'bowler cap'];
+// for i
+for (let i = 0; i < hatList.length; i++) {
+  // perform code
+  console.log(hatList[i]);
+}
+
+// for in
+for (let itemIndex in hatList) {
+  console.log('itemIndex:', itemIndex);
+}
+
+// for of
+for (let itemValue of hatList) {
+  console.log('itemValue:', itemValue);
+}
+
+// FUNCTIONS
+
+// function declaration
+function doubleIt(magicNumber) {
+  // code block
+  // what happens when called
+
+  // optional
+  return magicNumber * 2;
+}
+const valueBack = doubleIt(3);
+
+console.log('valueBack:', valueBack);
+
+// function expression
+const halveIt = function (aNumber) {
+  return aNumber / 2;
+};
+
+console.log('valueBack:', halveIt(22));
